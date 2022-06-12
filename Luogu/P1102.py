182f9d44fbcb490cd366e600966fa4c0fc60efe2
Xingjian Xu <xingjianxu@outlook.com>
2022-06-12 23:22:02 +0800

    Update P1102.py

--- Luogu/P1102.py
+++ Luogu/P1102.py
@@ -1,21 +1,30 @@
 n, c = map(int, input().split())
 num_arr = list(map(int, input().split()))
 
-num_arr.sort()
-counted_arr = []
-
-curr_val = num_arr[0]
-curr_cnt = 0
+deduped = {}
 for i in num_arr:
-    if i == curr_val:
-        curr_cnt += 1
+    if i not in deduped.keys():
+        deduped[i] = 1
     else:
-        counted_arr.append((curr_val, curr_cnt))
-        curr_val = i
-        curr_cnt = 1
-counted_arr.append((curr_val, curr_cnt))
-num_arr = counted_arr
-n = len(num_arr)
+        deduped[i] += 1
+num_arr = list(deduped.keys())
+num_arr.sort()
+n = len(deduped)
+
+# print(num_arr)
+# print(n)
+# curr_val = num_arr[0]
+# curr_cnt = 0
+# for i in num_arr:
+#     if i == curr_val:
+#         curr_cnt += 1
+#     else:
+#         counted_arr.append((curr_val, curr_cnt))
+#         curr_val = i
+#         curr_cnt = 1
+# counted_arr.append((curr_val, curr_cnt))
+# num_arr = counted_arr
+# n = len(num_arr)
 
 rslt = 0
 
@@ -30,13 +39,13 @@ rslt = 0
 curr = 0
 last = 0
 while curr < n:
-    if num_arr[curr][0] - num_arr[last][0] < c:
+    if num_arr[curr] - num_arr[last] < c:
         curr += 1
-    elif num_arr[curr][0] - num_arr[last][0] > c:
+    elif num_arr[curr] - num_arr[last] > c:
         last += 1 
     else:
         # print(last, curr)
-        rslt += num_arr[curr][1] * num_arr[last][1]
+        rslt += deduped[num_arr[curr]] * deduped[num_arr[last]]
         curr += 1
 
 print(rslt)
